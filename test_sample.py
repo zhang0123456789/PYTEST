7b91eb88316f0ca7c0bd7528a926c5df2059b7b5
1402686685 <1402686685@qq.com>
2019-01-07 18:01:19 +0800

    Initial commit

---
+++ test_sample.py
@@ -0,0 +1,12 @@
+#！/usr/bin/env python
+#_*_conding:dtf-8_*_
+# @Time    : 2019/1/7 0007 下午 17:56
+# @Author  : 蜜蜜
+# @Email   : 1402686685@qq.com
+# @File    : test_sample.py
+
+def func(x):
+    return  x+1
+
+def test_answer():
+    assert  func(3)==5
